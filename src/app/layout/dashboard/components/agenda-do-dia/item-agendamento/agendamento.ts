631f71c24fa48ea7618bf98a5188113e4d1e5e81
Luis Fernando de Barros <luis-barros@defensoria.rs.def.br>
2021-06-02 16:30:18 -0300

    add modal detalhes agendamento

--- src/app/layout/dashboard/components/agenda-do-dia/item-agendamento/agendamento.ts
+++ src/app/layout/dashboard/components/agenda-do-dia/item-agendamento/agendamento.ts
@@ -1,11 +1,13 @@
 export class Agendamento {
+    id: number;
     horario: string;
     nome: string;
     servico: string;
     valor: number;
     telefone: string;
 
-    constructor(horario: string, nome: string, servico: string, valor: number, telefone: string){
+    constructor(id: number, horario: string, nome: string, servico: string, valor: number, telefone: string){
+        this.id = id;
         this.horario = horario;
         this.nome = nome;
         this.servico = servico;
