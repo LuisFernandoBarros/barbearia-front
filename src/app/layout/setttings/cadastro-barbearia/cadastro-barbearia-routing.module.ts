847335cc678f71b0bbefae9a028bfd43d72fb009
Luis Fernando de Barros <luis-barros@defensoria.rs.def.br>
2021-06-08 19:48:10 -0300

    add menu e component barbearia

---
+++ src/app/layout/setttings/cadastro-barbearia/cadastro-barbearia-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CadastroBarbeariaComponent } from './component/cadastro-barbearia.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: CadastroBarbeariaComponent
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class CadastroBarbeariaRoutingModule {}
---
+++ src/app/layout/setttings/cadastro-barbearia/component/cadastro-barbearia.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-barbearia',
+  templateUrl: './cadastro-barbearia.component.html',
+  styleUrls: ['./cadastro-barbearia.component.css']
+})
+export class CadastroBarbeariaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
