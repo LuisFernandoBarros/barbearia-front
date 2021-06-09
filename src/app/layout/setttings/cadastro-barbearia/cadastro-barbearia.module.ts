ce18ae4412655e4f59876a07a6c544f5c7a4393d
Luis Fernando de Barros <luis-barros@defensoria.rs.def.br>
2021-06-08 21:10:55 -0300

    add form cadastro barbearia

--- src/app/layout/setttings/cadastro-barbearia/cadastro-barbearia.module.ts
+++ src/app/layout/setttings/cadastro-barbearia/cadastro-barbearia.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CadastroBarbeariaRoutingModule } from './cadastro-barbearia-routing.module';
 import { CadastroBarbeariaComponent } from './component/cadastro-barbearia.component';
 
 
 @NgModule({
-    imports: [CommonModule, CadastroBarbeariaRoutingModule],
+    imports: [CommonModule, CadastroBarbeariaRoutingModule,FormsModule, ReactiveFormsModule],
     declarations: [CadastroBarbeariaComponent]
 })
 export class CadastroBarbeariaModule {}
