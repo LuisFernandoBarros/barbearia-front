1c2d0f16ebe2d993695ad7e559b4e3f287b2278c
Luis Fernando de Barros <luis-barros@defensoria.rs.def.br>
2021-06-12 17:57:32 -0300

    add component editar servicos

--- src/app/layout/setttings/cadastro-servicos/cadastro-servicos-routing.module.ts
+++ src/app/layout/setttings/cadastro-servicos/cadastro-servicos-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastroServicosComponent } from './component/cadastro-servicos/cadastro-servicos.component';
+import { EditarComponent } from './component/editar/editar.component';
 
 const routes: Routes = [
     {
         path: '',
         component: CadastroServicosComponent
+    },
+    {
+        path: ':id/editar', component: EditarComponent
     }
 ];
 
@@ -13,4 +17,4 @@ const routes: Routes = [
     imports: [RouterModule.forChild(routes)],
     exports: [RouterModule]
 })
-export class CadastroServicosRoutingModule {}
+export class CadastroServicosRoutingModule { }
