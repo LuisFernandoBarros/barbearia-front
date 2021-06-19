2c944f05a12e0914c25a374bd38875e77f85956c
Luis Fernando de Barros <luis-barros@defensoria.rs.def.br>
2021-06-19 13:24:00 -0300

    add guarda a rotas filhas

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '../shared';
 import { LayoutComponent } from './layout.component';
 
 const routes: Routes = [
@@ -10,19 +11,44 @@ const routes: Routes = [
             { path: '', redirectTo: 'dashboard', pathMatch: 'prefix' },
             {
                 path: 'dashboard',
-                loadChildren: () => import('./dashboard/dashboard.module').then((m) => m.DashboardModule)
+                loadChildren: () => import('./dashboard/dashboard.module').then((m) => m.DashboardModule),
+                canActivate: [AuthGuard]
             },
-            { path: 'barbearia', loadChildren: () => import('./setttings/cadastro-barbearia/cadastro-barbearia.module').then((m) => m.CadastroBarbeariaModule) },
-            { path: 'profissional', loadChildren: () => import('./setttings/cadastro-profissional/cadastro-profissional.module').then((m) => m.CadastroProfissionalModule) },
-            { path: 'servicos', loadChildren: () => import('./setttings/cadastro-servicos/cadastro-servicos.module').then((m) => m.CadastroServicosModule) },
-            { path: 'charts', loadChildren: () => import('./charts/charts.module').then((m) => m.ChartsModule) },
-            { path: 'tables', loadChildren: () => import('./tables/tables.module').then((m) => m.TablesModule) },
-            { path: 'forms', loadChildren: () => import('./form/form.module').then((m) => m.FormModule) },
+            {
+                path: 'barbearia',
+                loadChildren: () => import('./setttings/cadastro-barbearia/cadastro-barbearia.module').then((m) => m.CadastroBarbeariaModule),
+                canActivate: [AuthGuard]
+            },
+            {
+                path: 'profissional',
+                loadChildren: () => import('./setttings/cadastro-profissional/cadastro-profissional.module').then((m) => m.CadastroProfissionalModule),
+                canActivate: [AuthGuard]
+            },
+            {
+                path: 'servicos',
+                loadChildren: () => import('./setttings/cadastro-servicos/cadastro-servicos.module').then((m) => m.CadastroServicosModule),
+                canActivate: [AuthGuard]
+            },
+            {
+                path: 'charts',
+                loadChildren: () => import('./charts/charts.module').then((m) => m.ChartsModule)
+            },
+            {
+                path: 'tables',
+                loadChildren: () => import('./tables/tables.module').then((m) => m.TablesModule)
+            },
+            {
+                path: 'forms',
+                loadChildren: () => import('./form/form.module').then((m) => m.FormModule)
+            },
             {
                 path: 'bs-element',
                 loadChildren: () => import('./bs-element/bs-element.module').then((m) => m.BsElementModule)
             },
-            { path: 'grid', loadChildren: () => import('./grid/grid.module').then((m) => m.GridModule) },
+            {
+                path: 'grid',
+                loadChildren: () => import('./grid/grid.module').then((m) => m.GridModule)
+            },
             {
                 path: 'components',
                 loadChildren: () => import('./bs-component/bs-component.module').then((m) => m.BsComponentModule)
