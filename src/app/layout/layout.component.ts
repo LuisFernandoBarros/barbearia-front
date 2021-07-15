4bc486b281b2882ad50eb35453d42cefa206a57c
Luis Fernando de Barros <luis-barros@defensoria.rs.def.br>
2021-07-15 20:21:28 -0300

    remove intro service

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { IntroService } from '../shared/services/intro/intro.service';
-import { LocalStorageService } from '../shared/services/local-storage.service';
 import { ResolutionService } from '../shared/services/resolution.service';
 
 @Component({
@@ -13,14 +11,11 @@ export class LayoutComponent implements OnInit {
 
     public isMobile = false;
 
-    constructor(private introService: IntroService,
-        private localStorageService: LocalStorageService,
-        private resolutionService: ResolutionService) { }
+    constructor(
+        private resolutionService: ResolutionService
+    ) { }
 
     ngOnInit() {
-        if (this.localStorageService.hasConfig('CADASTRAR_BARBEARIA')) {
-            this.introService.show('CADASTRAR_BARBEARIA');
-        }
         this.isMobile = this.resolutionService.isMobile();
     }
 
