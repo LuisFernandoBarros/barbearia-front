0cb77a4f406a1118808a3b2713f58cdd46629832
Luis Fernando de Barros <luis-barros@defensoria.rs.def.br>
2021-09-19 14:05:35 -0300

    insere botao de agendamento manual

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
     production: false,
-    API: "https://localhost:8050",
-    DOMINIO: "https://localhost:4200"
+    API: "http://localhost:8050",
+    DOMINIO: "http://localhost:4200"
 };
 
 /*
